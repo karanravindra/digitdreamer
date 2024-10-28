066a70a6692fc9ceac32c458dade5b69e14ab03f
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-10-28 16:56:55 -0400

    add rf

--- src/digitdreamer/modules/__init__.py
+++ src/digitdreamer/modules/__init__.py
@@ -2,3 +2,4 @@ from .attention import AttentionBlock
 from .block import Block
 from .mlp import MLP
 from .scales import Down, Up
+from .rf import RF
---
+++ src/digitdreamer/modules/rf.py
@@ -0,0 +1,42 @@
+import torch
+
+class RF:
+    def __init__(self, model, ln=True) -> None:
+        self.model = model
+        self.ln = ln
+
+    def forward(self, x, cond):
+        b = x.size(0)
+        if self.ln:
+            nt = torch.randn((b, 1), device=x.device)
+            t = torch.sigmoid(nt)
+        else:
+            t = torch.rand((b, 1), device=x.device)
+
+        texp = t.view([b, *([1] * len(x.shape[1:]))])
+        z1 = torch.randn_like(x)
+        zt = (1 - texp) * x + texp * z1
+        vtheta = self.model(zt, t, cond)
+        batchwise_mse = ((z1 - x - vtheta) ** 2).mean(dim=list(range(1, len(x.shape))))
+        tlist = batchwise_mse.detach().cpu().reshape(-1).tolist()
+        ttloss = list(zip(t, tlist))
+        return batchwise_mse.mean(), ttloss
+
+    @torch.no_grad()
+    def sample(self, z, cond, null_cond=None, sample_steps=50, cfg=2.0):
+        b = z.size(0)
+        dt = 1.0 / sample_steps
+        dt = torch.tensor([dt] * b).to(z.device).view([b, *([1] * len(z.shape[1:]))])
+        images = [z]
+        for i in range(sample_steps, 0, -1):
+            t = i / sample_steps
+            t = torch.tensor([t] * b, device=z.device)
+
+            vc = self.model(z, t, cond)
+            if null_cond is not None:
+                vu = self.model(z, t, null_cond)
+                vc = vu + cfg * (vc - vu)
+
+            z = z - dt * vc
+            images.append(z)
+        return images
